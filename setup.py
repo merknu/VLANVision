fd5b5168d1c76db24a9788434dba43e4171d89b2
merknu <8459791+merknu@users.noreply.github.com>
2023-04-29 12:49:17 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+# Path: setup.py
+#
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="vlanvision",
+    version="0.1.0",
+    author="Knut Ingmar Merødningen",
+    author_email="merknu@gmail.com",
+    description="A network management software that simplifies managing networks with powerful integrations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/vlanvision",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "vlanvision=vlanvision.main:main",
+        ],
+    },
+)
